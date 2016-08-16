3f8da11548dc929afb572644db2e3bcb3b695f2f
Alex Deaconu <alex.deaconu@gmail.com>
2016-08-16 13:47:16 -0400

    Add watch for img variable

--- dist/svg.js
+++ dist/svg.js
@@ -56,23 +56,24 @@ angular.module('risevision.common.svg.icons', [])
 
 angular.module('risevision.common.svg', ['risevision.common.svg.icons'])
   .directive('svgIcon', ['iconsList', function(iconsList) {
-    function link(scope, element, attrs) {
-      function path(icon) {
-        return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">' + 
-        '<path d="' + iconsList.icons1[icon] + '"/>' + 
-        '<path d="' + iconsList.icons2[icon] + '"/>' +
-        '</svg>';
-      }
-
-      function renderSVG() {
-        element.html( path(attrs.p) );
-      }
-      
-      renderSVG();
-    }
-
     return {
-      link: link,
-      restrict: 'E'
+      restrict: 'E',
+      scope: {
+        p: '@',
+      },
+      link: function($scope, element) {
+        function path(icon) {
+          return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">' + 
+          '<path d="' + iconsList.icons1[icon] + '"/>' + 
+          '<path d="' + iconsList.icons2[icon] + '"/>' +
+          '</svg>';
+        }
+
+        $scope.$watch('p', function(p) {
+          if (p) {
+            element.html( path(p) );
+          }
+        });
+      }
     };
   }]);
